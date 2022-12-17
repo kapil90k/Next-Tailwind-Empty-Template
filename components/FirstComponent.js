1252d2f83219ffea2c0c3ed9a543a9fd2a1b853f
kapil <kapil.mnnit90@gmail.com>
2022-12-17 23:21:27 +0800

    First Commit

---
+++ components/FirstComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function FirstComponent() {
+  return (
+    <div className="bg-yellow-400">
+      <p className="text-center font-bold text-red-900">
+        This is Starter Component
+      </p>
+    </div>
+  );
+}
+
+export default FirstComponent;
